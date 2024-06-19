3b686a248e4d8c11b81315065d54b170b11622c4
wenyongqd <wenyongqd@gmail.com>
2024-06-18 20:02:50 -0600

    make loading w-full

--- components/loading.tsx
+++ components/loading.tsx
@@ -6,7 +6,7 @@ export function Loading() {
       <Skeleton className="h-[38px] w-[90px]" />
       <Skeleton className="h-[38px] w-[80px]" />
     </div>
-    <div className="mx-auto w-[800px] space-y-6">
+    <div className="mx-auto w-full space-y-6">
       <Skeleton className="h-[50px] w-full" />
       <Skeleton className="h-[20px] w-2/3" />
       <Skeleton className="h-[20px] w-2/3" />
